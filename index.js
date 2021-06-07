ec782f860dd389962546bd970277457420883e35
Renat <kasymov.2002@mail.ru>
2021-06-07 08:57:38 +0300

    first commit

---
+++ index.js
@@ -0,0 +1,35 @@
+const express = require("express");
+const path = require("path");
+
+const PORT = process.env.PORT || 3000;
+
+const app = express();
+
+app.set("view engine", "ejs");
+app.set("views", path.join(__dirname, "ejs")); 
+console.log(app.get("view engine"));
+console.log(app.get("views"));
+
+// app.use(express.static(path.join(__dirname, "public")))
+
+app.get("/", (req, res) => {
+    res.render("index", {title: "Main page1", menu: {
+        "/about": "About us1",
+        "/contact": "Contact1"}})
+})
+
+app.get("/about", (req, res) => {
+    res.render("about", {title: "About us page1", menu: {
+        "/": "Main page2",
+        "/contact": "Contact2"}})
+})
+
+app.get("/contact", (req, res) => {
+    res.render("contact", {title: "Contact page1", menu: {
+        "/": "Main page3",
+        "/about": "About us3"}})
+})
+
+app.listen(3000, () => {
+    console.log(`Порт ${PORT}`);
+});
